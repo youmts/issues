aa626fb228823b51f176f6581711d657f9dc089e
You Matsumoto <anata0814@gmail.com>
2018-08-13 17:56:46 +0900

    Add project documents

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config,
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/youmts/issues",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -24,6 +26,8 @@ defmodule Issues.MixProject do
     [
       { :httpoison, "~> 0.8" },
       { :poison,    "~> 1.5"},
+      { :ex_doc,    "~> 0.11"},
+      { :earmark,   "~> 1.2"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
