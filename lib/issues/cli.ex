525c3ce0b8501407b5f75943bc474909d8628b71
You Matsumoto <anata0814@gmail.com>
2018-08-13 17:19:49 +0900

    Modify for packaging

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,6 +9,12 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
+  def main(argv) do
+    argv
+      |> parse_args
+      |> process
+  end
+
   def run(argv) do
     argv
     |> parse_args
